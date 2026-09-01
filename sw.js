a9940085352ba299001eaea94500ef59c0eab73c
SHOHIN HAMROH <iskandarusupov90@gmail.com>
2026-09-01 10:44:57 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,47 @@
-н
+const CACHE_NAME = "shohin-market-v1";
+
+const APP_FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./css/style.css",
+  "./css/components.css",
+  "./js/app.js",
+  "./js/products.js",
+  "./js/cart.js",
+  "./js/favorites.js",
+  "./js/orders.js",
+  "./js/map.js",
+  "./js/storage.js",
+  "./data/products.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_FILES))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(cached => {
+        return cached || fetch(event.request);
+      })
+      .catch(() => caches.match("./index.html"))
+  );
+});
